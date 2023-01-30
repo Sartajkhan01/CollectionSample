863db28e00c3172103c536d18d7963d92b56eec8
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-30 22:18:49 +0530

    [Sartaj] : added Collections Operation Program

---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CollectionsDemo;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to c# collection!");
+ Demo.doListDemo();
+ Demo.doStackDemo();
+Demo.doDictionaryDemo();
+ Demo.doQueueDemo();
+Demo.doSetDemo();
+Console.ReadKey();
